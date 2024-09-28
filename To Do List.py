fe2e0914e9a309e24aff761c9f50cd7535f8a746
UditSharma15 <sharmaudit1511@gmail.com>
2024-09-28 18:22:45 +0530

    To Do List.py

--- To Do List.py
+++ To Do List.py
@@ -5,6 +5,33 @@ print("2. Delete a task")
 print("3. View the Task")
 print("4. Exit")
 
+def addtask():
+  task = input("enter the task to add: ")
+  tasks.append(task)
+  print(f"the task {task} is successfully addded")
+
+def deltask():
+  tasktodelete = int(input("enter the number of the task you want to delete: ")
+  tasks.pop(tasktodelete)
+  print(f"the task #{tasktodelete} is successfully deleted")
+
+def viewtask():
+  if not task:
+    print("task list is empty")
+  else:
+    for index,task in enumerate(tasks):
+      print(f"Task #{index}. {task}")
+      
 while True:
-  
+  if choice == 1:
+    addtask()
+  elif choice == 2:
+    deltask()
+  elif choice == 3:
+    viewtask()
+  elif choice == 4:
+    break
+  else:
+    print("Invalid Input!)
+print("GoodBye!")
 
