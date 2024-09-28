ae5412cfc60269bf09815fa65328b35739051003
UditSharma15 <sharmaudit1511@gmail.com>
2024-09-28 17:35:18 +0530

    Create To Do List.py

---
+++ To Do List.py
@@ -0,0 +1,10 @@
+tasks= []
+print("Enter the number of the poeration you want to operate")
+print("1. Add  a task")
+print("2. Delete a task")
+print("3. View the Task")
+print("4. Exit")
+
+while True:
+  
+
