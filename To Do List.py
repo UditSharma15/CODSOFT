d2aa146a53372f8f89753cba3d3d5b4665bdb857
UditSharma15 <sharmaudit1511@gmail.com>
2024-10-17 23:14:38 +0530

    Update To Do List.py

--- To Do List.py
+++ To Do List.py
@@ -1,5 +1,5 @@
 tasks= []
-print("Enter the number of the poeration you want to operate")
+print("Enter the number of the operation you want to operate")
 print("1. Add  a task")
 print("2. Delete a task")
 print("3. View the Task")
